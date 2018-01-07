9029d893c593e96a6a6f0c647bed1b3e08dbbac2
pushnparekh <pushnparekh@gmail.com>
2018-01-07 16:17:15 +0530

    Add files via upload

---
+++ bang.js
@@ -0,0 +1,666 @@
+var bangaloretotal = [
+  {
+    "Name": "Banashankari",
+    "total": 276
+  },
+  {
+    "Name": "Bannerghatta Road",
+    "total": 369
+  },
+  {
+    "Name": "Bellandur",
+    "total": 262
+  },
+  {
+    "Name": "Brigade Road",
+    "total": 129
+  },
+  {
+    "Name": "BTM",
+    "total": 487
+  },
+  {
+    "Name": "Church Street",
+    "total": 54
+  },
+  {
+    "Name": "Frazer Town",
+    "total": 180
+  },
+  {
+    "Name": "HSR",
+    "total": 467
+  },
+  {
+    "Name": "Indiranagar",
+    "total": 488
+  },
+  {
+    "Name": "JP Nagar",
+    "total": 356
+  },
+  {
+    "Name": "Kammanahalli",
+    "total": 168
+  },
+  {
+    "Name": "Koramangala",
+    "total": 406
+  },
+  {
+    "Name": "Malleshwaram",
+    "total": 226
+  },
+  {
+    "Name": "Marathahalli",
+    "total": 529
+  },
+  {
+    "Name": "MG Road",
+    "total": 78
+  },
+  {
+    "Name": "New BEL Road",
+    "total": 274
+  },
+  {
+    "Name": "Old Airport Road",
+    "total": 141
+  },
+  {
+    "Name": "Residency Road",
+    "total": 70
+  },
+  {
+    "Name": "Whitefield",
+    "total": 734
+  }
+];
+
+var ahmedabadtotal = [
+  {
+    "Name": "Ambavadi",
+    "total": 72
+  },
+  {
+    "Name": "Ashram Road",
+    "total": 80
+  },
+  {
+    "Name": "Bodakdev",
+    "total": 262
+  },
+  {
+    "Name": "Bopal",
+    "total": 183
+  },
+  {
+    "Name": "C G Road",
+    "total": 99
+  },
+  {
+    "Name": "Ellis Bridge",
+    "total": 56
+  },
+  {
+    "Name": "Ghatlodia",
+    "total": 39
+  },
+  {
+    "Name": "Gota",
+    "total": 84
+  },
+  {
+    "Name": "Gulbai Tekra",
+    "total": 30
+  },
+  {
+    "Name": "Gurukul",
+    "total": 77
+  },
+  {
+    "Name": "Infocity",
+    "total": 74
+  },
+  {
+    "Name": "Lal Darwaja",
+    "total": 26
+  },
+  {
+    "Name": "Navrangpura",
+    "total": 94
+  },
+  {
+    "Name": "Prahlad Nagar",
+    "total": 133
+  },
+  {
+    "Name": "Satellite",
+    "total": 314
+  },
+  {
+    "Name": "Sola",
+    "total": 97
+  },
+  {
+    "Name": "Thaltej",
+    "total": 149
+  },
+  {
+    "Name": "Vasna",
+    "total": 16
+  },
+  {
+    "Name": "Vastrapur",
+    "total": 232
+  },
+  {
+    "Name": "Vejalpur",
+    "total": 39
+  }
+];
+ 
+ var chennaitotal = [{
+    "Name": "Adyar",
+    "total": 174
+  },
+  {
+    "Name": "Alwarpet",
+    "total": 103
+  },
+  {
+    "Name": "Anna Nagar East",
+    "total": 195
+  },
+  {
+    "Name": "Ashok Nagar",
+    "total": 102
+  },
+  {
+    "Name": "Besant Nagar",
+    "total": 113
+  },
+  {
+    "Name": "Gopalapuram",
+    "total": 62
+  },
+  {
+    "Name": "Kilpauk",
+    "total": 150
+  },
+  {
+    "Name": "Kodambakkam",
+    "total": 100
+  },
+  {
+    "Name": "Mylapore",
+    "total": 139
+  },
+  {
+    "Name": "Nungambakkam",
+    "total": 249
+  },
+  {
+    "Name": "Perungudi",
+    "total": 135
+  },
+  {
+    "Name": "Porur",
+    "total": 166
+  },
+  {
+    "Name": "Ramapuram",
+    "total": 98
+  },
+  {
+    "Name": "Royapettah",
+    "total": 98
+  },
+  {
+    "Name": "T. Nagar",
+    "total": 293
+  },
+  {
+    "Name": "Thuraipakkam",
+    "total": 128
+  },
+  {
+    "Name": "Vadapalani",
+    "total": 105
+  },
+  {
+    "Name": "Velachery",
+    "total": 239
+  }];
+  
+  var delhitotal = [
+  {
+    "Name": "Connaught Place",
+    "total": 310
+  },
+  {
+    "Name": "DLF Phase 4",
+    "total": 247
+  },
+  {
+    "Name": "Delhi University-GTB Nagar",
+    "total": 164
+  },
+  {
+    "Name": "Golf Course Road",
+    "total": 184
+  },
+  {
+    "Name": "Greater Kailash 1 (GK1)",
+    "total": 165
+  },
+  {
+    "Name": "Hauz Khas",
+    "total": 86
+  },
+  {
+    "Name": "Janpath",
+    "total": 66
+  },
+  {
+    "Name": "Karol Bagh",
+    "total": 259
+  },
+  {
+    "Name": "Khan Market",
+    "total": 57
+  },
+  {
+    "Name": "Netaji Subhash Place",
+    "total": 117
+  },
+  {
+    "Name": "Rajouri Garden",
+    "total": 314
+  },
+  {
+    "Name": "Saket",
+    "total": 320
+  },
+  {
+    "Name": "Sector 18 Noida",
+    "total": 240
+  },
+  {
+    "Name": "Sector 38 Noida",
+    "total": 115
+  },
+  {
+    "Name": "Vasant Kunj",
+    "total": 349
+  },
+  {
+    "Name": "Vijay Nagar",
+    "total": 400
+  }
+];
+
+ var hybdtotal = [
+  {
+    "Name": "Abids",
+    "total": 97
+  },
+  {
+    "Name": "Ameerpet",
+    "total": 107
+  },
+  {
+    "Name": "Banjara Hills",
+    "total": 407
+  },
+  {
+    "Name": "Begumpet",
+    "total": 110
+  },
+  {
+    "Name": "Gachibowli",
+    "total": 443
+  },
+  {
+    "Name": "Himayath Nagar",
+    "total": 119
+  },
+  {
+    "Name": "Hitech City",
+    "total": 216
+  },
+  {
+    "Name": "Jubilee Hills",
+    "total": 233
+  },
+  {
+    "Name": "Kondapur",
+    "total": 248
+  },
+  {
+    "Name": "Kukatpally",
+    "total": 411
+  },
+  {
+    "Name": "Madhapur",
+    "total": 402
+  },
+  {
+    "Name": "Tolichowki",
+    "total": 99
+  }
+];
+
+var indoretotal = [
+  {
+    "Name": "Bhawar Kuan",
+    "total": 89
+  },
+  {
+    "Name": "New Palasia",
+    "total": 90
+  },
+  {
+    "Name": "Old Palasia",
+    "total": 97
+  },
+  {
+    "Name": "RNT Marg",
+    "total": 48
+  },
+  {
+    "Name": "Sapna Sangeeta",
+    "total": 45
+  },
+  {
+    "Name": "South Tukoganj",
+    "total": 54
+  },
+  {
+    "Name": "VijayNagar",
+    "total": 306
+  }
+];
+
+var jaipurtotal = [
+  {
+    "Name": "C Scheme",
+    "total": 184
+  },
+  {
+    "Name": "Malviya Nagar",
+    "total": 310
+  },
+  {
+    "Name": "Mansarovar",
+    "total": 265
+  },
+  {
+    "Name": "MI Road",
+    "total": 122
+  },
+  {
+    "Name": "Raja Park",
+    "total": 140
+  },
+  {
+    "Name": "Tonk Road",
+    "total": 234
+  },
+  {
+    "Name": "Vaishali Nagar",
+    "total": 198
+  }
+];
+
+var kolkatatotal = [
+  {
+    "Name": "Ballygunge",
+    "total": 184
+  },
+  {
+    "Name": "Behala",
+    "total": 194
+  },
+  {
+    "Name": "Bhawanipur",
+    "total": 111
+  },
+  {
+    "Name": "Garia",
+    "total": 132
+  },
+  {
+    "Name": "Jadavpur",
+    "total": 114
+  },
+  {
+    "Name": "Kankurgachi",
+    "total": 123
+  },
+  {
+    "Name": "Kasba",
+    "total": 138
+  },
+  {
+    "Name": "New Market Area",
+    "total": 117
+  },
+  {
+    "Name": "Park Street Area",
+    "total": 132
+  },
+  {
+    "Name": "Rajarhat New Town",
+    "total": 293
+  },
+  {
+    "Name": "Sector 1, Salt Lake",
+    "total": 191
+  },
+  {
+    "Name": "Sector 3, Salt Lake",
+    "total": 101
+  },
+  {
+    "Name": "Sector 5, Salt Lake",
+    "total": 147
+  },
+  {
+    "Name": "Tollygunge",
+    "total": 142
+  }
+];
+
+var lucknowtotal = [
+  {
+    "Name": "Alambagh",
+    "total": 176
+  },
+  {
+    "Name": "Aliganj",
+    "total": 177
+  },
+  {
+    "Name": "GomtiNagar",
+    "total": 395
+  },
+  {
+    "Name": "Hazratganj",
+    "total": 181
+  },
+  {
+    "Name": "Indira Nagar",
+    "total": 162
+  }
+];
+
+
+var mumbaitotal = [
+  {
+    "Name": "Andheri",
+    "total": 290
+  },
+  {
+    "Name": "Bandra Kurla Complex",
+    "total": 107
+  },
+  {
+    "Name": "Borivali West",
+    "total": 444
+  },
+  {
+    "Name": "Chakala",
+    "total": 191
+  },
+  {
+    "Name": "Chembur",
+    "total": 423
+  },
+  {
+    "Name": "Colaba",
+    "total": 153
+  },
+  {
+    "Name": "Fort",
+    "total": 209
+  },
+  {
+    "Name": "Goregaon East",
+    "total": 255
+  },
+  {
+    "Name": "Bandra West",
+    "total": 261
+  },
+  {
+    "Name": "Juhu",
+    "total": 228
+  },
+  {
+    "Name": "Kandivali East",
+    "total": 271
+  },
+  {
+    "Name": "Kandivali West",
+    "total": 331
+  },
+  {
+    "Name": "Khar",
+    "total": 174
+  },
+  {
+    "Name": "Lower Parel",
+    "total": 358
+  },
+  {
+    "Name": "Mahakali",
+    "total": 226
+  },
+  {
+    "Name": "Malad West",
+    "total": 526
+  },
+  {
+    "Name": "Mira Road",
+    "total": 516
+  },
+  {
+    "Name": "Mulund West",
+    "total": 295
+  },
+ 
+  {
+    "Name": "Powai",
+    "total": 323
+  },
+  {
+    "Name": "Vashi",
+    "total": 385
+  },
+  {
+    "Name": "Worli",
+    "total": 121
+  }
+];
+
+var punetotal = [
+  {
+    "Name": "Aundh",
+    "total": 200
+  },
+  {
+    "Name": "Baner",
+    "total": 327
+  },
+  {
+    "Name": "Camp Area",
+    "total": 151
+  },
+  {
+    "Name": "Chinchwad",
+    "total": 244
+  },
+  {
+    "Name": "Hadapsar",
+    "total": 301
+  },
+  {
+    "Name": "Hinjawadi",
+    "total": 321
+  },
+  {
+    "Name": "Kalyani Nagar",
+    "total": 166
+  },
+  {
+    "Name": "Kharadi",
+    "total": 310
+  },
+  {
+    "Name": "Koregaon Park",
+    "total": 180
+  },
+  {
+    "Name": "Kothrud",
+    "total": 403
+  },
+  {
+    "Name": "Magarpatta",
+    "total": 169
+  },
+  {
+    "Name": "NIBM Road",
+    "total": 141
+  },
+  {
+    "Name": "Pimple Saudagar",
+    "total": 236
+  },
+  {
+    "Name": "Pimpri",
+    "total": 222
+  },
+  {
+    "Name": "Sadashiv Peth",
+    "total": 132
+  },
+  {
+    "Name": "FC Road",
+    "total": 85
+  },
+  {
+    "Name": "Sinhgad Road",
+    "total": 285
+  },
+  {
+    "Name": "Viman Nagar",
+    "total": 386
+  },
+  {
+    "Name": "Wakad",
+    "total": 312
+  }
+];
+
+
